fe2947bcc27df359f9e6ee800a729a3dfb7537d6
EAGLEEARTH <ufukcicek199@gmail.com>
2023-11-24 15:22:10 +0300

    push

--- timesharehub/urls.py
+++ timesharehub/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
 
 
     # Diğer uygulama URL'leri...
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, settings.MEDIA_URL, document_root=settings.STATIC_ROOT)
