6d7b996bbd160997723919b806647ead0bb82d43
EAGLEEARTH <ufukcicek199@gmail.com>
2023-11-24 14:44:21 +0300

    push

--- timesharehub/settings.py
+++ timesharehub/settings.py
@@ -147,8 +147,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
+
     os.path.join(BASE_DIR, 'static_files')
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
