c5bfc6f0adbdb40074b22f8570c428b17f0d36f9
EAGLEEARTH <ufukcicek199@gmail.com>
2023-11-25 13:29:16 +0300

    push

--- myapp/views.py
+++ myapp/views.py
@@ -54,7 +54,7 @@ def upload_counter_view(request):
 
 
 def custom_404_view(request, exception):
-    return render(request, 'errors/404.html', status=404)
+    return render(request, 'upload/404.html', status=404)
 
 def custom_500_view(request):
-    return render(request, 'errors/500.html', status=500)
+    return render(request, 'upload/500.html', status=500)
