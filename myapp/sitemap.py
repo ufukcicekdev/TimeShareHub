89f741bef8c443ab4012fb9680677f435dad5195
EAGLEEARTH <ufukcicek199@gmail.com>
2023-11-26 17:11:14 +0300

    push

---
+++ myapp/sitemap.py
@@ -0,0 +1,19 @@
+from django.urls import reverse
+from django.contrib.sitemaps import Sitemap
+from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
+startdate = datetime.today()
+enddate = startdate + relativedelta(years=10)
+
+class MainLinkSiteMap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.8
+    protocol = 'https'
+
+    def items(self):
+        return ['timesharehub.online']
+
+    def location(self, obj):
+        # Ana URL'nin gerçek bir sayfa değeri olmadığından None döndürün
+        return None
--- myapp/urls.py
+++ myapp/urls.py
@@ -1,13 +1,22 @@
 from django.urls import path, reverse
 from .views import upload_document,view_document
 from django.views.generic import TemplateView
+from django.contrib.sitemaps.views import sitemap
+from myapp.sitemap import *
 
 app_name = 'myapp'
 
 
+sitemaps = {
+    'mainLinkSiteMap':MainLinkSiteMap, 
+}
+
 urlpatterns = [
     path('', upload_document, name='upload_document'),
     path('document/<uuid:document_uuid>/', view_document, name='view_document'),
+    path('sitemap.xml/', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
+
     path('robots.txt',TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),
+    
     # Diğer URL tanımları...
 ]
